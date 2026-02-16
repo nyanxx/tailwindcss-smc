1c236c7a59a36a1a7c47f7517a9f79bfb8c0c238
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-02-16 16:21:06 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { JSX } from "react"
+
+export default function App(): JSX.Element {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-blue-600">
+        Hello, Tailwind!
+      </h1>
+      <p className="text-gray-700 mt-2">
+        This is your first Tailwind-styled page.
+      </p>
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer transition"> Get Started </button>
+    </>
+  )
+}
